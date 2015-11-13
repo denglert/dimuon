5756935a3c562262d95550a51f6f5b1a1d3b607e
denglert <englert.david@gmail.com>
2015-11-13 12:39:08 +0000

    Book and display histogram

--- dimuon.py
+++ dimuon.py
@@ -1,4 +1,8 @@
-from ROOT import TFile, TBrowser
+#
+# Plot dimuon mass distribution for SWC HEP 2015
+#
+from ROOT import TFile, TBrowser, TH1D
+
 
 file_events = TFile("test_data/events.root")
 tree = file_events.Get("events")
@@ -6,8 +10,15 @@ nEvents = tree.GetEntries()
 print 'Number of events = '+ str(nEvents)
 
 # xrange(n) = 0,1,2,...,n-1
-for iEv in xrange(nEvents):
-    tree.GetEntry(iEv)
-    nParticles = tree.nPart
-    print 'Number of particle = ' + str(nParticles)
+#for iEv in xrange(nEvents):
+#    tree.GetEntry(iEv)
+#    nParticles = tree.nPart
+#    print 'Number of particle = ' + str(nParticles)
 
+hist_dimuon_mass_nBins = 100
+hist_dimuon_mass_massMin = 0
+hist_dimuon_mass_massMax = 500
+
+h = TH1D("histo_dimuon_mass","di-muon mass spectrum;m [GeV^{2}/c];Entries", hist_dimuon_mass_nBins, hist_dimuon_mass_massMin, hist_dimuon_mass_massMax )
+h.Draw()
+raw_input("Press return to exit.")
