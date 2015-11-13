a28073fa4825765a4d590a94c1bbfb7316f806e3
denglert <englert.david@gmail.com>
2015-11-13 12:17:56 +0000

    Loop over events; print number of particles

--- dimuon.py
+++ dimuon.py
@@ -2,5 +2,12 @@ from ROOT import TFile, TBrowser
 
 f = TFile("test_data/events.root")
 t = f.Get("events")
-n = t.GetEntries()
-print 'Number of events = '+ str(n)
+nEvents = t.GetEntries()
+print 'Number of events = '+ str(nEvents)
+
+# xrange(n) = 0,1,2,...,n-1
+for iEv in xrange(nEvents):
+    t.GetEntry(iEv)
+    nParticles = t.nPart
+    print 'Number of particle = ' + str(nParticles)
+
